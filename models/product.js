30a0e68afc3240c3985b954affcee338e15be8fe
Mubassir Ali <mubassir11@gmail.com>
2024-04-24 16:47:05 +0500

    fixed average rating for all product

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { HOST, PORT } = require('../utils/constants')
+const Reviews = require('./productReview');
 
 
 // If the model does not exist, create it
@@ -40,6 +41,11 @@ const productSchema = Schema(
             ref: 'ProductReview'
         }],
 
+        averageRating: {
+            type: Number,
+            default: 0
+        },
+
         createdBy: {
             type: Schema.Types.ObjectId, ref: 'WholeSeller'
         },
@@ -52,30 +58,67 @@ const productSchema = Schema(
     }
 );
 
-// Define a pre-find hook to add base URL to each image URL
-productSchema.post('find', function (docs) {
+productSchema.post('find', async function (docs) {
+    // Array to store all promises
+    const promises = [];
+    
     docs.forEach(product => {
-        const imagesWithUrls = product.images.map(image => `${HOST}:${PORT}/images/product/${image}`)
+        // Check if images URLs already have the prefix added
+        const imagesWithUrls = product.images.map(image => {
+            if (!image.startsWith(`${HOST}:${PORT}/images/product/`)) {
+                return `${HOST}:${PORT}/images/product/${image}`;
+            } else {
+                return image; // URL already has the prefix, no need to append again
+            }
+        });
+        
         product.images = imagesWithUrls;
+
+        if (product.reviews && product.reviews.length > 0) {
+            // Push each asynchronous operation into the promises array
+            promises.push(
+                Reviews.find({_id: {$in: product.reviews}}, { rating: 1 })
+                    .then(reviews => {
+                        reviews = reviews.map(item => item.rating);
+                        console.log('reviews::::::::::::::::', reviews);
+                        const sum = reviews.reduce((total, rating) => total + rating, 0);
+                        product.averageRating = sum / reviews.length;
+                    })
+            );
+        }
     });
+
+    // Wait for all asynchronous operations to complete
+    await Promise.all(promises);
 });
 
-productSchema.post('findOne', function(doc) {
-      doc.images = doc.images.map(image => `${HOST}:${PORT}/images/product/${image}`)
-  });
+
+productSchema.post('findOne', async function(doc) {
+    if (doc) {
+        // Transform images URLs
+        doc.images = doc.images.map(image => {
+            // Check if the URL prefix has already been added
+            if (!image.startsWith(`${HOST}:${PORT}/images/product/`)) {
+                return `${HOST}:${PORT}/images/product/${image}`;
+            } else {
+                return image; // URL already has the prefix, no need to append again
+            }
+        });
+        
+        // Process reviews and calculate average rating if needed
+        if (doc.reviews && doc.reviews.length > 0) {
+            const reviews = await Reviews.find({_id: {$in: doc.reviews}}, { rating: 1 });
+            const ratings = reviews.map(item => item.rating);
+            const sum = ratings.reduce((total, rating) => total + rating, 0);
+            doc.averageRating = sum / ratings.length;
+        }
+    }
+});
 
 
 productSchema.virtual('payableAmount').get(function () {
     return this.price.amount - (this.price.amount * this.discountInPercent / 100);
 });
-
-productSchema.virtual('averageRating').get(function() {
-    if (this.reviews && this.reviews.length > 0) {
-      const totalRating = this.reviews.reduce((acc, review) => acc + review.rating, 0);
-      return totalRating / this.reviews.length;
-    }
-    return 0; // Return 0 if no reviews are available
-  });
 
 
 productSchema.set('toJSON', { virtuals: true });
